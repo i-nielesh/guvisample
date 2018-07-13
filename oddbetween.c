a6f9c568fad46ceb1cacdec0d89f0c0dd40a72d7
Neeleshk26 <40922513+Neeleshk26@users.noreply.github.com>
2018-07-13 22:04:38 +0530

    Create oddbetween.c

---
+++ oddbetween.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int a,b,i;
+  printf("enter smaller no.");
+  scanf("%d",&a);
+  printf("enter larger no.");
+  scanf("%d",&b);
+  for(i=a;i<b;i++){
+    if(i%2!=0)
+      printf("%d\t",i);
+  }
+  return 0;
+}
