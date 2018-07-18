2e22c7a47b1b58f8be33450289e7f28b149e27d0
Neeleshk26 <40922513+Neeleshk26@users.noreply.github.com>
2018-07-18 22:56:12 +0530

    Update maxarr.c

--- maxarr.c
+++ maxarr.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int a[100],i,n,max;
+    int a[100],i,n,max=0;
     printf("enter number");
     scanf("%d",&n);
     printf("enter elements in array");
@@ -8,9 +8,9 @@ int main(){
         scanf("%d",&a[i]);
     }
      for(i=0;i<n-1;i++){
-         if(a[i]>a[i+1])
+         if((a[i]>a[i+1])&&(a[i]>max))
             max=a[i];
-        else
+        else if(a[i+1]>max)
             max=a[i+1];
      }
      printf("maximum number is:%d",max);
