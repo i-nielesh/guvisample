4bc5cc09fbfdb474eea2e80ac53305e295997bef
Neeleshk26 <40922513+Neeleshk26@users.noreply.github.com>
2018-08-08 10:45:59 -0700

    Create numstr.c

---
+++ numstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i,l,flag=0;
+    char str[100];
+    scanf("%s",str);
+    l=strlen(str);
+    for(i=0;i<l;i++){
+        if(str[i]>=48&&str[i]<=57){
+            flag=1;
+            continue;
+        }
+        else{
+            flag=0;
+            break;
+        }
+    }
+    if(flag==1)
+        printf("yes");
+    else if(flag==0)
+        printf("no");
+    return 0;
+}
