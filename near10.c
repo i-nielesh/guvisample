481b29d2514813174b0491894920cc0348d0ad6f
Neeleshk26 <40922513+Neeleshk26@users.noreply.github.com>
2018-09-07 15:45:08 +0530

    Update near10.c

--- near10.c
+++ near10.c
@@ -5,13 +5,7 @@ int main()
 	int n,r;
 	scanf("%d",&n);
 	r=n%10;
-    if(r<5){
-        n=n-r;
-	    printf("%d",n);
-    }
-    else{
-        n=n+(10-r);
-        printf("%d",n);
-    }
+    n=n+10-r;
+    printf("%d",n);
 	return 0;
 }
